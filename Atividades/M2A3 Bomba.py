ad49c92ba1883dd5d03e641b8ff7a92a1e0324dc
Davi Thorpe <davithorpe2@gmail.com>
2022-07-21 11:38:58 -0300

    Adicionando M2A3 Bomba
    Atividade 3 do modulo 2

---
+++ Atividades/M2A3 Bomba.py
@@ -0,0 +1,13 @@
+#Instruções do projeto
+#Faça um código que execute a contagem regressiva de uma bomba, informando o número de segundos para explodir. Ele deverá mostrar a mensagem“iniciando contagem regressiva”, os segundos passados e, no final, a mensagem “BUM!”.
+
+#Não é necessário, mas, caso deseje tornar o sistema mais realista para que o tempo realmente passe em segundos, você pode usar a função time.sleep() que existe na Python (acesse o link em anexo). No entanto, é preciso adicionar uma biblioteca antes de executá-la.
+
+#Inicio do código
+
+print('Iniciando contagem regressiva')
+from time import sleep
+for reg in range(10, -1, -1):
+    print(reg)
+    sleep(1)
+print('BUM!')
