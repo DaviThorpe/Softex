a80067ba88a90958460cf319e8f8d083d2544863
Davi Thorpe <davithorpe2@gmail.com>
2022-12-13 15:05:18 -0300

    Update Atividade 10 - Funções.sql

--- Modulo de banco de dados/Atividade 10 - Funções.sql
+++ Modulo de banco de dados/Atividade 10 - Funções.sql
@@ -1,3 +1,10 @@
+/* 
+Crie uma função que some todos os clientes cadastrados em uma loja durante um dia.
+*/
+
+create database loja;
+use loja;
+
 create table clientes(
 nome varchar (30),
 registro int not null auto_increment,
@@ -15,6 +22,20 @@ values
 
 select * from clientes;
 
-SELECT COUNT(registro) as cadastros_dia , dtCadastro 
-from clientes
-group by dtCadastro;
+delimiter $$
+
+CREATE FUNCTION fn_soma () RETURNS INT
+BEGIN
+	
+    declare Soma, Retorno int;
+    
+    SELECT COUNT(registro) as cadastros_dia , dtCadastro into Soma 
+	from clientes;
+    
+	RETURN Soma;
+    
+END $$
+
+delimiter ; 
+
+select`loja`.`fn_soma`();
