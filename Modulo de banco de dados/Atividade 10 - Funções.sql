16b0ac8daed063f1dbd8c03be9e32c15a3155523
Davi Thorpe <109236792+DaviThorpe@users.noreply.github.com>
2022-12-14 11:39:13 -0300

    Add files via upload

--- Modulo de banco de dados/Atividade 10 - Funções.sql
+++ Modulo de banco de dados/Atividade 10 - Funções.sql
@@ -1,3 +1,4 @@
+
 /* 
 Crie uma função que some todos os clientes cadastrados em uma loja durante um dia.
 */
@@ -20,22 +21,39 @@ values
 ('Paula', '2022-09-19'),
 ('Lucas', '2022-09-20');
 
+insert into clientes
+(nome, dtCadastro)
+values
+('Laura', '2022-12-14'),
+('Leo', '2022-12-14'),
+('Menó', '2022-12-14'),
+('Valença', '2022-12-14'),
+('Luciana', '2022-12-14');
+
 select * from clientes;
+
+/*A função não funciona de um modo realmente prático, precisa de algum ajuste para aceitar qualquer data
+no momento ela irá esta somando todos os cadastros independete do dia*/
 
 delimiter $$
 
-CREATE FUNCTION fn_soma () RETURNS INT
+CREATE FUNCTION fn_soma (registro int, dtCadastro date) RETURNS INT
 BEGIN
 	
-    declare Soma, Retorno int;
-    
-    SELECT COUNT(registro) as cadastros_dia , dtCadastro into Soma 
-	from clientes;
-    
+declare Soma int;
+if dtCadastro = curdate() then	
+	SELECT count(registro) from clientes where dtCadastro = curdate()
+    into Soma;
 	RETURN Soma;
     
+else
+	set Soma = '1';    
+    RETURN Soma;
+end if;
+        
 END $$
 
 delimiter ; 
 
-select`loja`.`fn_soma`();
+select fn_soma(registro, dtCadastro) as clientes_cadastrados, dtCadastro from clientes
+where dtCadastro = curdate(); 
