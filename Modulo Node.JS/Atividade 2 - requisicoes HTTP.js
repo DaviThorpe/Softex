fe91570b61829a5d50c2dba7b627d62fbee52f76
Davi Thorpe <davithorpe2@gmail.com>
2023-01-28 08:36:01 -0300

    Update Atividade 2 - requisicoes HTTP.js

--- Modulo Node.JS/Atividade 2 - requisicoes HTTP.js
+++ Modulo Node.JS/Atividade 2 - requisicoes HTTP.js
@@ -1,3 +1,6 @@
+//Sem usar bibliotecas, crie um projeto simples em Node.js que seja capaz de responder requisições HTTP.
+//Explique como rodar e testar.
+
 const http = require('http');
 
 const server = http.createServer((request, response) => {
@@ -10,3 +13,6 @@ server.listen(8080, () => {
 });
 
 //Aqui está um exemplo de um servidor HTTP básico que retorna uma mensagem de "Olá, mundo!" para todas as requisições
+
+// Para rodar o código abra o terminal ou o prompt de comando e navegue até a pasta do seu projeto usando o comando cd.
+//Após isso basta digitar o comando "node" seguido do nome do arquivo que deseja executar.
