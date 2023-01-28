5a536e3bfcd4edbb3b32a895b9282d3348ac3835
Davi Thorpe <davithorpe2@gmail.com>
2023-01-28 08:36:06 -0300

    Create Atividade 5 receber quatro tipos de requisições.js

---
+++ Modulo Node.JS/Atividade 5 receber quatro tipos de requisições.js
@@ -0,0 +1,27 @@
+// Crie um código Node.JS usando a biblioteca Express. 
+//Nele, você deve receber quatro tipos de requisições e responder cada uma com strings diferentes. 
+//Por fim, explique como rodar o código.
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+  response.send('Recebi uma requisição GET');
+});
+
+app.post('/', (request, response) => {
+  response.send('Recebi uma requisição POST');
+});
+
+app.put('/', (request, response) => {
+  response.send('Recebi uma requisição PUT');
+});
+
+app.delete('/', (request, response) => {
+  response.send('Recebi uma requisição DELETE');
+});
+
+app.listen(8080, () => {
+  console.log('Servidor iniciado na porta 8080');
+});
