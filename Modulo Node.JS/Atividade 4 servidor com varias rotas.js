8f4bf991b99ecb8ea4256c58e0ec9231f1802d5b
Davi Thorpe <davithorpe2@gmail.com>
2023-01-28 08:36:05 -0300

    Update Atividade 4 servidor com varias rotas.js

--- Modulo Node.JS/Atividade 4 servidor com varias rotas.js
+++ Modulo Node.JS/Atividade 4 servidor com varias rotas.js
@@ -1,10 +1,14 @@
+//Crie um pequeno servidor web que contenha várias rotas.
+//Em cada rota, você deverá fazer o controle do fluxo de requisições de maneira estática ou dinâmica, 
+//alterne entre essas formas usando placeholders e query.
+
 const express = require('express');
 
 const app = express();
 
 // Rota estática que responde a requisições GET na rota raiz
 app.get('/', (request, response) => {
-  response.send('Recebi uma requisição GET na rota raiz');
+  response.send('Recebi uma requisição GET');
 });
 
 // Rota dinâmica que usa um placeholder para aceitar um ID de usuário
@@ -15,9 +19,9 @@ app.get('/usuarios/:id', (request, response) => {
 
 // Rota que usa o objeto request.query para ler os parâmetros da query string
 app.get('/produtos', (request, response) => {
-  const nome = request.query.nome;
-  const preco = request.query.preco;
-  response.send(`Recebi uma requisição GET para o produto com nome ${nome} e preço ${preco}`);
+  const usuario = request.query.usuario;
+  const id = request.query.id;
+  response.send(`Recebi uma requisição GET para o produto com usario ${usuario} e seu id ${id}`);
 });
 
 app.listen(8080, () => {
