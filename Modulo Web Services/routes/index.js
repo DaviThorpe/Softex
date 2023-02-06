6a97f1f69eba6bcae36fd5db653dc3827989eb21
Davi Thorpe <davithorpe2@gmail.com>
2023-02-06 14:31:42 -0300

    Create index.js

---
+++ Modulo Web Services/routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const jogos = require("./jogosRoutes.js")
+
+const routes = (app) => {
+    app.route('/').get((req, res) => {
+        res.status(200).send({titulo: "Curso de node"})
+    })
+
+    app.use(
+        express.json(),
+        jogos
+    )
+}
+
+modeule.exports = routes
